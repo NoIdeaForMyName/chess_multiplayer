125c6da9d7ce3375f347da3cba417e9ab82ea773
NoIdeaForMyName <maksanty.michal@gmail.com>
2024-06-01 19:01:12 +0200

    Created module for serialization

---
+++ serialize.py
@@ -0,0 +1,21 @@
+# import json
+#
+#
+# def receive_data(data) -> dict:
+#     return json.loads(data.decode('utf-8'))
+#
+#
+# def send_data(data):
+#     return json.dumps(data).encode('utf-8')
+
+import pickle
+
+
+def receive_data(data) -> dict:
+    return pickle.loads(data)
+
+
+def send_data(data):
+    return pickle.dumps(data)
+
+
