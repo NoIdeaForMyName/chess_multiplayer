08882f73d49a66d1fab15e48ddb4869246a60859
NoIdeaForMyName <maksanty.michal@gmail.com>
2024-06-03 21:35:36 +0200

    Added option for forcing end of the game (useful if one of the players disconnects which is tantamount to giving up)

--- game.py
+++ game.py
@@ -45,6 +45,8 @@ class Game:
 
     def __init__(self, player_color: Color, white_player: str, black_player: str, time: float) -> None:
 
+        self._chess_game = ChessBoard()
+
         self._player_color = player_color
         self._another_player_move: tuple[tuple[int, int]] | None = None
         self._all_move_list: list[tuple[tuple[int, int]]] = []
@@ -72,6 +74,8 @@ class Game:
             Color.Black: ChessClock(time)
         }
         self.active_clock = self.clocks[Color.White]
+
+        self._top_down_ending_winner = False
 
         sounds_path = 'resources\\sounds'
         self.move_sound = pygame.mixer.Sound(os.path.join(sounds_path, 'move.mp3'))
@@ -132,29 +136,30 @@ class Game:
 
         checked_king_pos = None
 
-        chess_game = ChessBoard()
-        board = chess_game.board
+        board = self._chess_game.board
 
         game_lasts = True
         self._game_state = GameState.InProgress
 
-        while game_lasts:
+        while game_lasts and not self._top_down_ending_winner:
             #print('game_loop')
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
+                    self._winner = self.players[Color.White if self._player_color != Color.White else Color.Black]
+                    self._game_state = GameState.Ended
                     pygame.quit()
                     sys.exit()
-                elif event.type == pygame.MOUSEBUTTONDOWN and self._player_color == chess_game.turn:
+                elif event.type == pygame.MOUSEBUTTONDOWN and self._player_color == self._chess_game.turn:
                     if event.button == 1:  # Left mouse button
                         row, col = self.get_cell_under_mouse()
-                        if not isinstance(board[row][col], EmptyPiece) and board[row][col].color == chess_game.turn:
+                        if not isinstance(board[row][col], EmptyPiece) and board[row][col].color == self._chess_game.turn:
                             dragging_piece = board[row][col]
                             dragging_piece_pos = (row, col)
                             dragging_piece_rect = self.piece_images[str(dragging_piece)].get_rect(center=pygame.mouse.get_pos())
                             # board[row][col] = None
-                elif (event.type == pygame.MOUSEBUTTONUP and self._player_color == chess_game.turn and event.button == 1 and dragging_piece or
-                self._player_color != chess_game.turn and self.another_player_move is not None):
-                    if self._player_color == chess_game.turn:
+                elif (event.type == pygame.MOUSEBUTTONUP and self._player_color == self._chess_game.turn and event.button == 1 and dragging_piece or
+                self._player_color != self._chess_game.turn and self.another_player_move is not None):
+                    if self._player_color == self._chess_game.turn:
                         old_row, old_col = dragging_piece_pos
                         row, col = self.get_cell_under_mouse()
                     else:
@@ -162,7 +167,7 @@ class Game:
                         row, col = self.another_player_move[1]
                         self.another_player_move = None
 
-                    move_type, check_state = chess_game.move((old_row, old_col), (row, col))
+                    move_type, check_state = self._chess_game.move((old_row, old_col), (row, col))
 
                     if move_type == MoveType.InvalidMove:
                         self.wrong_move.play()  # invalid move sound
@@ -182,7 +187,7 @@ class Game:
                             checked_king_pos = None
                         case CheckState.Check:
                             self.check_sound.play()
-                            checked_king_pos = chess_game.find_king(chess_game.white_king if board[row][col].color == Color.Black else chess_game.black_king)
+                            checked_king_pos = self._chess_game.find_king(self._chess_game.white_king if board[row][col].color == Color.Black else self._chess_game.black_king)
                         case CheckState.Checkmate:
                             self.game_end_sound.play()
                             game_lasts = False  # end game
@@ -213,12 +218,21 @@ class Game:
             self.clock.tick(60)
 
         self._game_state = GameState.Ended
-        self._winner = self.players[chess_game.winner]
+        if not self._top_down_ending_winner:
+            self._winner = self.players[self._chess_game.winner]
+        else:
+            print('top down:', self._top_down_ending_winner)
+            self._winner = self._top_down_ending_winner
+        self.display_ending()
 
+    def display_ending(self):
         while not (pygame.QUIT in [e.type for e in pygame.event.get()]):
-            self.display_winner(chess_game.winner)
+            self.display_winner(self._winner)
             pygame.display.flip()
             self.clock.tick(60)
+
+    def top_down_game_ending(self, winner: str):
+        self._top_down_ending_winner = winner
 
     def switch_clocks(self) -> ChessClock:
         self.active_clock.refresh_time()
@@ -245,17 +259,26 @@ class Game:
         self.screen.blit(text_white, text_white_rect)
         self.screen.blit(text_black, text_black_rect)
 
-    def display_winner(self, winner: Color):
+    def display_winner(self, winner: str):
         font = pygame.font.SysFont('Comic Sans MS', 72)
-        text = font.render(f'{winner.name} wins!', False, (255, 255, 255) if winner == Color.White else (0, 0, 0))
+        text = font.render(f'{winner} wins!', False, (0, 0, 255))
         text_rect = text.get_rect(center=(self.SCREEN_WIDTH // 2, self.SCREEN_HEIGHT // 2))
         self.screen.blit(text, text_rect)
 
 
 def main():
+    import threading, time
+    def foo(g):
+        time.sleep(5)
+        print('SETTING WINNER')
+        g.top_down_game_ending('Mac')
     game = Game(Color.White, 'Mic', 'Mac', 600)
+    t = threading.Thread(target=foo, args=(game,))
+    t.start()
     game.start()
+    #game.top_down_game_ending('Mac')
     print(game.winner)
+    t.join()
 
 
 if __name__ == "__main__":
