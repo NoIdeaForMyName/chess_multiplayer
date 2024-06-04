db963e978e1c5b721075c90851486f5c44cd04ab
NoIdeaForMyName <maksanty.michal@gmail.com>
2024-06-04 02:08:57 +0200

    Added handling of json data type for serialization purposes

--- serialize.py
+++ serialize.py
@@ -9,6 +9,7 @@
 #     return json.dumps(data).encode('utf-8')
 
 import pickle
+import json
 
 
 def receive_data(data):
@@ -19,3 +20,12 @@ def send_data(data):
     return pickle.dumps(data)
 
 
+def write_data_to_file(data, filename):
+    with open(filename, 'w') as file:
+        json.dump(data, file)
+
+
+def read_data_from_file(filename):
+    with open(filename, 'r') as file:
+        data = json.load(file)
+    return data
